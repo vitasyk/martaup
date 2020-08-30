e2ef84a1dedeb365c7b24002d23d9869f36ea80c
vitasyk <lifelinesecret@gmail.com>
2020-08-30 13:17:21 +0300

    PC First

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.header__slider').slick({
+        arrows: false,
+        vertical: true,
+        dots: true,
+        dotsClass: 'header-dots',
+        autoplay: 2000,
+        // fade: true,
+    });
+
+});
